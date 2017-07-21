b5df2c3fde9091d109de837a3cdb18b7c81a2718
verluchie <30330189+verluchie@users.noreply.github.com>
2017-07-21 08:42:04 +0700

    Update example.py

--- example.py
+++ example.py
@@ -13,8 +13,8 @@ from instabot import InstaBot
 from unfollow_protocol import unfollow_protocol
 
 bot = InstaBot(
-    login="elbasrie",
-    password="asungentot12",
+    login="login",
+    password="password",
     like_per_day=1000,
     comments_per_day=0,
     tag_list=['follow4follow', 'f4f', 'cute'],
